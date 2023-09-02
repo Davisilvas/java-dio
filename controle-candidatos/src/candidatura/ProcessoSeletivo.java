c57f52fbb003f82081a84e588eae11f2449113ff
Davisilvas <morg.daviss@gmail.com>
2023-09-02 15:24:03 -0300

    parte 01 do desafio do Bootcamp Java DIO

--- controle-candidatos/src/candidatura/ProcessoSeletivo.java
+++ controle-candidatos/src/candidatura/ProcessoSeletivo.java
@@ -1,22 +1,82 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.Random;
+
 public class ProcessoSeletivo {
     public static void main(String[] args) {
-        analisarCadidato(1850.0);
-        analisarCadidato(2000.0);
-        analisarCadidato(2200.0);
+        String [] candidates = {"Davi", "Maria", "Doly", "Nina", "Tom", "Mila", "Princesa"};
+        for(String candidate : candidates){
+            gettingInTouch(candidate);
+        }
     }
 
+    static void gettingInTouch(String candidate){
+        int attempts = 1;
+        boolean keepTrying = true;
+        boolean answered = false;
+
+        do{
+            answered = answerTheCall();
+            keepTrying = !answered;
+
+            if(keepTrying){
+                attempts++;
+            }else{
+                System.out.println("The candidate answerd the call.");
+            }
+
+        } while(keepTrying && attempts <= 3);
+
+        if(answered){
+            System.out.printf("We achieved contact with %s at %d attempt. %n", candidate, attempts);
+        }else{
+            System.out.printf("We couldn't get in touch with %s. We achived our limit of attempts %d.%n", candidate, attempts);
+        }
+
+    }
     
+    static boolean answerTheCall(){
+       return new Random().nextInt(3)==1;
+    }
+
+    static void printSelectedCandidates(){
+        String [] candidates = {"Davi", "Maria", "Doly", "Nina", "Tom", "Mila", "Princesa"};
+
+        System.out.println("Imprimindo candidatos selecionados");
+
+        for(int i = 0; i < candidates.length; i++){
+            System.out.printf("The Candidate number %d is %s%n", (i + 1), candidates[i]);
+        }
+    }
     
-    /**
-     * METHOD analisarCandidato.
-     * @param expectedSalary -> this value represents the amount that the candidate wishes to have as his salary.
-     * this method will help sorting candidates. This method separetes into higher base salary, equal base salary and lower than base salary.
-     */
+    static void pickingCandidates(){
+        String [] candidates = {"Davi", "Maria", "Doly", "Nina", "Tom", "Mila", "Princesa", "Thor", "Luke", "Tânia", "Artur", "Pedro", "Happy", "Jerry"};
+
+        double baseSalary = 2000.0;
+        int selectedCandidates = 0;
+        int actualCandidate = 0;
+
+        while (selectedCandidates < 5 && actualCandidate < candidates.length){
+            String candidate = candidates[actualCandidate];
+            double expectedSalary = randomExpectedSalary();
+
+            System.out.printf("The candidate %s requested the following value for his salary %.2f%n", candidate, expectedSalary);
+            
+            if(baseSalary >= expectedSalary){
+                System.out.printf("**The candidate %s was selected for the job.**%n", candidate);
+                selectedCandidates++;
+            }
+
+            actualCandidate++;
+        }
+    }
+
+    static double randomExpectedSalary(){
+        return ThreadLocalRandom.current().nextDouble(1850.0, 2200.0);
+    }
     
-    static void analisarCadidato(double expectedSalary){
-        
+    static void analyzingCandidate(double expectedSalary){
         double baseSalary = 2000.0;
         if(baseSalary > expectedSalary){
             System.out.println("Call to the candidate.");
